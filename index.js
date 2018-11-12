bbf7ae178b90c2316727120989c7e5202c743bbd
nick paridon <paridonn1@gmail.com>
2018-11-12 10:10:01 -0500

    cors

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'https://admodaltest.herokuapp.com/');
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
 
