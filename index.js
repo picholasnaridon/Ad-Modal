1e7b85acf757fafb720a62c8c7e41fae79abee2f
nick paridon <paridonn1@gmail.com>
2018-11-12 09:37:05 -0500

    port

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
+var express = require(‘express’);
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.get('*', function(req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000);
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
+});
