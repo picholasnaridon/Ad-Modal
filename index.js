770ce85e2da06431d6722e016e27bbbfd580cf47
nick paridon <paridonn1@gmail.com>
2018-11-19 12:05:04 -0500

    updates

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
 
-var port = process.env.PORT || 3000;
-var app = express();
+const port = process.env.PORT || 3000;
+const app = express();
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
 
-app.get('/', function(req, res, next) {
-	res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res, next) => {
+	res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(port, function() {
-	console.log(`Example app listening on port !`);
+app.listen(port, () => {
+	console.log('Example app listening on port !');
 });
